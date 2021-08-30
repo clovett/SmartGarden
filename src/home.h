f989d2a717eae1d6f8c0ad331d4c600312fbe80a
Chris Lovett <clovett@microsoft.com>
2021-08-30 01:12:35 -0700

    better iphone sizes

--- src/home.h
+++ src/home.h
@@ -1,8 +1,9 @@
 const char* homePage = "<!DOCTYPE HTML>  \r\n\
 <html>  \r\n\
+  <meta name='viewport' content='width=device-width, minimum-scale=1.0, maximum-scale=1.0' />  \r\n\
 <style>  \r\n\
 body { background-color: #1E1E1E; color: #D4D4D4;}  \r\n\
-button { min-width:80px; min-height:30px; }  \r\n\
+button { min-width:80px; min-height:30px;  -webkit-appearence: button; padding: 5px;}  \r\n\
 .big { font-size:48pt; border-style: solid; border-width: 1px; border-color: #D4D4D4; padding: 10px; }  \r\n\
 </style>  \r\n\
 <script>  \r\n\
@@ -17,21 +18,20 @@ function sendCommand(cmd) {  \r\n\
   xhr.send();  \r\n\
 }  \r\n\
 function read() { sendCommand('r'); }  \r\n\
-function slope() { sendCommand('slope'); }  \r\n\
 function info() { sendCommand('i'); }  \r\n\
 function status() { sendCommand('status'); }  \r\n\
 </script>  \r\n\
 <body>  \r\n\
-<h3>Smart Garden</h3>  \r\n\
+<h1>Smart Garden</h1>  \r\n\
+<span>  \r\n\
 <button onclick='read()'>Read</button>  \r\n\
-&nbsp;  \r\n\
-<button onclick='slope()'>Slope</button>  \r\n\
 &nbsp;  \r\n\
 <button onclick='info()'>Info</button>  \r\n\
 &nbsp;  \r\n\
 <button onclick='status()'>Status</button>  \r\n\
+</span>  \r\n\
 <br/><br/><br/>  \r\n\
-<span class='big' id='result'></span>  \r\n\
+<span class='big' id='result'>0.000</span>  \r\n\
 </body>  \r\n\
   \r\n\
 </html>  \r\n\
